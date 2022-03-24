4e74cdf62b103e365f42f5f9c8986542f57dccdb
zuzanapiorkowska <napiorkowskaz@gmail.com>
2022-03-24 14:58:40 +0100

    mocne wtf, ale stabilnie

--- src/Michałowe_src/niewiem.test.ts
+++ src/Michałowe_src/niewiem.test.ts
@@ -1,4 +1,10 @@
+import { appendFile, fstat } from "fs";
 import { ExchangeRates } from "./types/ExchangeRates";
+const nock = require("nock");
+import fetch from "node-fetch";
+import fs from "fs";
+import { json } from "stream/consumers";
+import { resolve as resolvePath } from "path";
 
 const a: ExchangeRates = {
   table: "",
@@ -12,7 +18,6 @@ const a: ExchangeRates = {
     },
   ],
 };
-//control + spacja wyświetlają podpowiedzi, a jak są na końcu, to strzałka do góry pozwala się do nich szybko dostać
 
 const b: ExchangeRates = {
   table: "",
@@ -28,15 +33,75 @@ const b: ExchangeRates = {
 };
 
 const calculateRateDifference = (a: ExchangeRates, b: ExchangeRates) => {
-  const rateA = a.rates[0].mid *4;
-  const rateB = b.rates[0].mid *4;
-  const result = +((rateA - rateB)/4).toFixed(2)
-  return result
+  const rateA = a.rates[0].mid * 1000;
+  const rateB = b.rates[0].mid * 1000;
+  //mnożymy *1000, żeby odejmowanie przeprowadzić na liczbach całkowitych, bo na takich po przecinku się wykrzaczy
+  const result = +((rateA - rateB) / 1000).toFixed(2);
+  //toFixed obcina, ale nie zaokrągla i zwraca wartość w stringu
+  return result;
 };
-//toFixed obcina, ale nie zaokrągla
+
+function saveRecordings(tapeName: string) {
+  return new Promise<void>((resolve) => {
+  fs.mkdir(resolvePath(__dirname, "__tapes__"), ()=> {
+    fs.writeFile(resolvePath(__dirname, "__tapes__", `${tapeName}.json`), JSON.stringify(nock.recorder.play()), ()=>{
+      //możemy tu zwrócić promisa jak to się wykona
+      resolve();
+    })
+  })
+  //podłogapodłoga sugeruje, że to dane do testów
+})
+}
+
+
+export async function saveRecordings2(dirName: string, tapeName: string) {
+try {
+  await fs.promises.mkdir(resolvePath(dirName, "__tapes3__"))
+}catch{}
+  //nowsze wersje node'a mają taką opcję, żeby zrobić to tak zamiast promise'a
+  await fs.promises.writeFile(
+    resolvePath(dirName, "__tapes3__", `${tapeName}.json`),
+    JSON.stringify(nock.recorder.play())
+  );
+}
 
 describe("exchange difference", () => {
-  it("correctly count difference between rates", () => {
+  // beforeEach(()=> nock.disableNetConnect());
+  //po dodaniu tylko i wyłącznie tego test się wywali, no bo nie ma neta
+  // afterEach(()=> nock.enableNetConnect());
+  it("correctly count difference between rates", async () => {
+    // nock.recorder.rec()
+    // takie coś nam powyżej nam zwróci loada w konsoli
+    nock.recorder.rec({output_objects: true, dont_print: true});
+    //dzięki zawartości obiektu dostajemy jsona => output_objects to sprawia
+    //z tym recorderem sobie puszczam testtylko raz, no bo ma się nagrać, a jak już jest nagrane, no to nie trza
+    //jak już się puści, hehe, to przekopiowujemy odpowiedź do oddzielnego pliku (u mnie exchangeRates.tape.json) i wrzucamy ścieżkę do niego w "load", poniżej
+    // nock.load(`${__dirname}/nocks/exchangeRates.tape.json`);
+    const response = await fetch(
+      "http://api.nbp.pl/api/exchangerates/rates/a/usd/2016-04-04/?format=json"
+    );
+    //fetcha możemy zostawić, bo on tu nic nie przeszkadza
+    const data = (await response.json()) as ExchangeRates;
+    //pobieranie data wynieść do oddzielnej funkcji by fajnie było
     expect(calculateRateDifference(a, b)).toBe(1.22);
+    await saveRecordings2(`${__dirname}`, "usd7"); //jest promisem, bo tworzenie i zapisywanie plików jest asunchroniczne i wymaga callcaków
+    console.log(nock.recorder.play())
+    //to nam pokaże wszystkie requesty z testu
   });
 });
+
+//request recording - robimy sobie request do API i zapisujemy, jakby "nagrywamy" sobie ten request, żeby później to pobieranie zafejkować
+//reqiesty i responsy można zamockować, ale często jest trudno, bo są skomplikowane, mogą mieź headery, mogą się wywalać, bo za długo, możgą zwracać string etc.
+//mockowanie tej zależności, którą jest to zewnętrzne api (ten request)jest bardzo trudne
+
+//zamiast tego możemy zrobić fetcha, bierzemy odpowiedź i zapiszmy to i z tego robimy plik z przykładowym API
+//później sprawdzamy czy poszedł z tymi samymi parametrami -> np. fetch z parametrami accept json. nie tworzymy raw requestu (w takiej formie)
+
+//biblioteka nock umożliwia recordowanie tych requestów
+
+// po requescie który róbimy, możemy sobie zrobić expecta
+
+// (await Response.json()) musi być w nawiasie przy typowanie "as....", bo inaczej byłoby
+
+//za pierwszym razem robimy request tak po prostu
+//a za drugim razem już taki fejkowy
